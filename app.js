fbf2bb367412949f86b2f646b86fa094d0964ba7
justin <justinrvillena@gmail.com>
2021-05-11 21:41:50 -0700

    inquirer prompt added

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+//const fs = require('fs');
+
+//const generatePage = require('./src/page-template.js');
+
+//const pageHTML = generatePage(name, github);
 
 const profileDataArgs = process.argv.slice(2);
 
 const [name, github] = profileDataArgs;    
     
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+//fs.writeFile('./index.html', generatePage(name, github), err => {
+//    if (err) throw new Error(err);
 
-    console.log('Portfolio complete! Check out index.html to see the ouput!');
-});
+//    console.log('Portfolio complete! Check out index.html to see the ouput!');
+//});
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+    
